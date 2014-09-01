f5b508f68a400aca022a176bb42006f4ffef2514
Ryan <bobcatzboy@gmail.com>
2014-09-01 18:20:45 -0400

    Finished the faction.

--- gamemode/schema/factions/sh_ussr.lua
+++ gamemode/schema/factions/sh_ussr.lua
@@ -1,13 +1,18 @@
 -- The 'nice' name of the faction.
-FACTION.name = "Soviet Union"
+FACTION.name = "Soviet Union Soldier"
 -- A description used in tooltips in various menus.
-FACTION.desc = "A standard soldier deployed from the "
+FACTION.desc = "A standard soldier deployed from the Soviet Union."
 -- A color to distinguish factions from others, used for stuff such as
 -- name color in OOC chat.
-FACTION.color = Color(70, 70, 220)
+FACTION.color = Color(163, 0, 0)
 -- Set the male model choices for character creation.
 FACTION.maleModels = {
-	"",
+	"models/soviet/soviet_soldier1.mdl",
+	"models/soviet/soviet_soldier2.mdl",
+	"models/soviet/soviet_soldier3.mdl",
+	"models/soviet/soviet_soldier4.mdl",
+	"models/soviet/soviet_soldier5.mdl",
+	"models/soviet/soviet_soldier6.mdl"
 
 }
 
@@ -18,9 +23,9 @@ FACTION.isDefault = false
 
 -- Return what the name will be set for character creation.
 --function FACTION:GetDefaultName()
---	return "CP-RCT."..math.random(11111, 99999)
+--	return "CP-RCT."..math.random(111, 999)
 --end
 
 -- FACTION.index is defined when the faction is registered and is just a numeric ID.
 -- Here, we create a global variable for easier reference to the ID.
-FACTION_TALI = FACTION.index
+FACTION_USSR = FACTION.index
